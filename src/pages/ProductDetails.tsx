e1829271188dd65527e5cc0e2c569ab66e4d15c6
myarask <myaraskavitch@gobonfire.com>
2021-02-08 20:43:33 -0500

    add currency

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -8,6 +8,7 @@ const ProductDetails = () => {
   const [product, setProduct] = useState<any>(null);
 
   useEffect(() => {
+    // This is a basic data-loading mechanism. React Query is a viable upgrade.
     Products.retrieve(id).then(setProduct);
   }, [id, setProduct]);
 
@@ -16,11 +17,26 @@ const ProductDetails = () => {
     return null;
   }
 
+  const { targetMarket, title, price } = product.data;
+
   return (
     <ProductGrid>
-      <div>1</div>
-      <div>product details {id}</div>
-      <div>1</div>
+      <section>1</section>
+      <section>product details {id}</section>
+      <article>
+        <section>
+          <span>{targetMarket}</span>
+          <h1>{title}</h1>
+          <h1>
+            {new Intl.NumberFormat("en-CA", {
+              maximumFractionDigits: 0,
+              minimumFractionDigits: 0,
+              style: "currency",
+              currency: price.currency,
+            }).format(price.amount)}
+          </h1>
+        </section>
+      </article>
     </ProductGrid>
   );
 };
--- src/services/products.ts
+++ src/services/products.ts
@@ -4,8 +4,11 @@ import { axios, mock } from "./_axios";
 mock.onGet("/products/1").reply(200, {
   id: 1,
   title: "Capri Triple White",
-  price: 26800, // Price in lowest currency denomination (cents)
-  currency: "CAD",
+  price: {
+    amount: 268,
+    currency: "CAD",
+  },
+  targetMarket: "Men's",
   description:
     "Any look, any day. (Every day?) In buttery full-grain calf leather, the men’s Capri Triple White sneakers are the ultimate wardrobe cornerstone. The one that lets you do you. These White Capri shoes are so neutral they freshen and compliment just about anything. These sneakers let you reinvent the fashion wheel, or keep the white capri classic. Rather than canvas, these white low-top sneakers are made with real Italian leather for only the best men’s capri look and wearability. Keep these kicks fresh or curiously banged up: your choice. ",
 });
